a3a8636e7e7d4ba2188362d4fad7b7f99d8bc9d8
vivian <vivian@eff.org>
2017-03-29 16:51:09 -0400

    Process single log entry

--- cryptolog.go
+++ cryptolog.go
@@ -7,15 +7,27 @@ import (
 	"crypto/hmac"
 	"crypto/md5"
 	"encoding/base64"
+	"regexp"
+)
+
+var (
+	salt = "ueErQYkQp5A9LrNbRQ1+XQ=="
 )
 
 func main() {
 }
 
-func hashIp(ip, salt string) string {
+func processSingleLogEntry(log_entry string) string {
+	ipv4_exp := `^(\d\d?\d?\.\d\d?\d?\.\d\d?\d?\.\d\d?\d?).*`
+	r, _ := regexp.Compile(ipv4_exp)
+	indexes := r.FindStringSubmatchIndex(log_entry)
+	hashedIP := hashIp(log_entry[indexes[2]:indexes[3]])
+	return log_entry[:indexes[2]] + hashedIP + log_entry[indexes[3]:]
+}
+
+func hashIp(ip string) string {
 	mac := hmac.New(md5.New, []byte(salt))
 	mac.Write([]byte(ip))
 	hashedIp := mac.Sum(nil)
 	return base64.StdEncoding.EncodeToString(hashedIp)[:6]
-	// return string(hashedIp)
 }
--- cryptolog_test.go
+++ cryptolog_test.go
@@ -4,9 +4,16 @@ import "testing"
 
 func TestHashIp(t *testing.T) {
 	ip := "172.17.0.1"
-	salt := "ueErQYkQp5A9LrNbRQ1+XQ=="
 	expected := "JKx+3b"
-	if got := hashIp(ip, salt); got != expected {
+	if got := hashIp(ip); got != expected {
+		t.Errorf("Expected %s, got %s", expected, got)
+	}
+}
+
+func TestProcessSingleLogEntry(t *testing.T) {
+	log_entry := `172.17.0.1 - - [29/Mar/2017:20:09:52 +0000] "GET / HTTP/1.1" 304 -`
+	expected := `JKx+3b - - [29/Mar/2017:20:09:52 +0000] "GET / HTTP/1.1" 304 -`
+	if got := processSingleLogEntry(log_entry); got != expected {
 		t.Errorf("Expected %s, got %s", expected, got)
 	}
 }
