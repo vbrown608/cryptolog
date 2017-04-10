50ebfd193988f48d4836531625406885e3a1bbad
vivian <vivian@eff.org>
2017-04-10 13:48:44 -0400

    Compile regex once

--- cryptolog.go
+++ cryptolog.go
@@ -2,21 +2,22 @@
 package main
 
 import (
-  "fmt"
-  "os"
+  "bufio"
   "crypto/hmac"
   "crypto/md5"
   "crypto/rand"
   "encoding/base64"
   "flag"
+  "fmt"
+  "os"
   "regexp"
   "time"
-  "bufio"
 )
 
 const (
-  ipv4_exp = `(\d\d?\d?\.\d\d?\d?\.\d\d?\d?\.\d\d?\d?)`
-  ipv6_exp = `(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))`
+  ipv4Exp = `(\d\d?\d?\.\d\d?\d?\.\d\d?\d?\.\d\d?\d?)`
+  ipv6Exp = `(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))`
+  ipExp = ipv4Exp + "|" + ipv6Exp
 )
 
 var (
@@ -28,12 +29,15 @@ func main() {
 `Set the lifetime of the hash salt.
 This is the duration during which the hashes of a given ip will be identical.
 See https://golang.org/pkg/time/#ParseDuration for format.` )
+  firstMatchOnly := flag.Bool("first-match-only", false, `Hash only the first IP in each log line.`)
   flag.Parse()
+
   go generateSalt(*saltLifetime)
 
+  r := compileRegexp(*firstMatchOnly)
   scanner := bufio.NewScanner(os.Stdin)
   for scanner.Scan() {
-    entry := processSingleLogEntry(scanner.Text())
+    entry := processSingleLogEntry(scanner.Text(), r)
     fmt.Println(entry)
   }
 }
@@ -45,10 +49,19 @@ func generateSalt(delay time.Duration) {
   }
 }
 
-func processSingleLogEntry(log_entry string) string {
-  r, _ := regexp.Compile(ipv4_exp + "|" + ipv6_exp)
-  hashed_entry := r.ReplaceAllStringFunc(log_entry, hashIp)
-  return hashed_entry
+func compileRegexp(firstMatchOnly bool) *regexp.Regexp {
+  if firstMatchOnly {
+    r, _ := regexp.Compile(`^(.*?)` + ipExp + `(.*)$`)
+    return r
+  } else {
+    r, _ := regexp.Compile(ipExp)
+    return r
+  }
+}
+
+func processSingleLogEntry(logEntry string, r *regexp.Regexp) string {
+  hashedEntry := r.ReplaceAllStringFunc(logEntry, hashIp)
+  return hashedEntry
 }
 
 func hashIp(ip string) string {
--- cryptolog_test.go
+++ cryptolog_test.go
@@ -3,21 +3,23 @@ package main
 import "testing"
 
 func TestHashIp(t *testing.T) {
-	salt = []byte("ueErQYkQp5A9LrNbRQ1+XQ==")
+  salt = []byte("ueErQYkQp5A9LrNbRQ1+XQ==")
 
-	ip := "172.17.0.1"
-	expected := "JKx+3b"
-	if got := hashIp(ip); got != expected {
-		t.Errorf("Expected %s, got %s", expected, got)
-	}
+  ip := "172.17.0.1"
+  expected := "JKx+3b"
+  if got := hashIp(ip); got != expected {
+    t.Errorf("Expected %s, got %s", expected, got)
+  }
 }
 
 func TestProcessSingleLogEntry(t *testing.T) {
-	salt = []byte("ueErQYkQp5A9LrNbRQ1+XQ==")
+  salt = []byte("ueErQYkQp5A9LrNbRQ1+XQ==")
+  r := compileRegexp(false)
 
-	log_entry := `172.17.0.1 - - [29/Mar/2017:20:09:52 +0000] "GET / HTTP/1.1" 304 -`
-	expected := `JKx+3b - - [29/Mar/2017:20:09:52 +0000] "GET / HTTP/1.1" 304 -`
-	if got := processSingleLogEntry(log_entry); got != expected {
-		t.Errorf("Expected %s, got %s", expected, got)
-	}
+
+  log_entry := `172.17.0.1 - - [29/Mar/2017:20:09:52 +0000] "GET / HTTP/1.1" 304 -`
+  expected := `JKx+3b - - [29/Mar/2017:20:09:52 +0000] "GET / HTTP/1.1" 304 -`
+  if got := processSingleLogEntry(log_entry, r); got != expected {
+    t.Errorf("Expected %s, got %s", expected, got)
+  }
 }
