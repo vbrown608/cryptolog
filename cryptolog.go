7457a3088b6f2e82be0e0b385d19c05d87e3db20
vivian <vivian@eff.org>
2017-04-11 18:30:35 -0400

    Test replace first match flag

--- cryptolog.go
+++ cryptolog.go
@@ -2,71 +2,71 @@
 package main
 
 import (
-  "bufio"
-  "crypto/hmac"
-  "crypto/md5"
-  "crypto/rand"
-  "encoding/base64"
-  "flag"
-  "fmt"
-  "os"
-  "regexp"
-  "time"
+	"bufio"
+	"crypto/hmac"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"os"
+	"regexp"
+	"time"
 )
 
 const (
-  ipv4Exp = `(\d\d?\d?\.\d\d?\d?\.\d\d?\d?\.\d\d?\d?)`
-  ipv6Exp = `(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))`
-  ipExp = ipv4Exp + "|" + ipv6Exp
+	ipv4Exp = `(\d\d?\d?\.\d\d?\d?\.\d\d?\d?\.\d\d?\d?)`
+	ipv6Exp = `(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))`
+	ipExp   = ipv4Exp + "|" + ipv6Exp
 )
 
 var (
-  salt = make([]byte, 10)
+	salt = make([]byte, 10)
 )
 
 func main() {
-  saltLifetime := flag.Duration("salt-lifetime", time.Hour*24,
-`Set the lifetime of the hash salt.
+	saltLifetime := flag.Duration("salt-lifetime", time.Hour*24,
+		`Set the lifetime of the hash salt.
 This is the duration during which the hashes of a given ip will be identical.
-See https://golang.org/pkg/time/#ParseDuration for format.` )
-  firstMatchOnly := flag.Bool("first-match-only", false, `Hash only the first IP in each log line.`)
-  flag.Parse()
+See https://golang.org/pkg/time/#ParseDuration for format.`)
+	replaceAll := flag.Bool("replace-all-matches", true, `Replace all occurences of IP addresses within the string.
+If false, only the first occurance with be replaced.`)
+	flag.Parse()
 
-  go generateSalt(*saltLifetime)
+	go generateSalt(*saltLifetime)
 
-  r := compileRegexp(*firstMatchOnly)
-  scanner := bufio.NewScanner(os.Stdin)
-  for scanner.Scan() {
-    entry := processSingleLogEntry(scanner.Text(), r)
-    fmt.Println(entry)
-  }
+	r := compileRegexp(*replaceAll)
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		entry := processSingleLogEntry(scanner.Text(), r)
+		fmt.Println(entry)
+	}
 }
 
 func generateSalt(delay time.Duration) {
-  for {
-    rand.Read(salt)
-    time.Sleep(delay)
-  }
+	for {
+		rand.Read(salt)
+		time.Sleep(delay)
+	}
 }
 
-func compileRegexp(firstMatchOnly bool) *regexp.Regexp {
-  if firstMatchOnly {
-    r, _ := regexp.Compile(`^(.*?)` + ipExp + `(.*)$`)
-    return r
-  } else {
-    r, _ := regexp.Compile(ipExp)
-    return r
-  }
+func compileRegexp(replaceAll bool) *regexp.Regexp {
+	if replaceAll {
+		r, _ := regexp.Compile(ipExp)
+		return r
+	}
+	r, _ := regexp.Compile(`^(.*?)` + ipExp + `(.*)$`)
+	return r
 }
 
 func processSingleLogEntry(logEntry string, r *regexp.Regexp) string {
-  hashedEntry := r.ReplaceAllStringFunc(logEntry, hashIp)
-  return hashedEntry
+	hashedEntry := r.ReplaceAllStringFunc(logEntry, hashIP)
+	return hashedEntry
 }
 
-func hashIp(ip string) string {
-  mac := hmac.New(md5.New, []byte(salt))
-  mac.Write([]byte(ip))
-  hashedIp := mac.Sum(nil)
-  return base64.StdEncoding.EncodeToString(hashedIp)[:6]
+func hashIP(ip string) string {
+	mac := hmac.New(md5.New, []byte(salt))
+	mac.Write([]byte(ip))
+	hashedIP := mac.Sum(nil)
+	return base64.StdEncoding.EncodeToString(hashedIP)[:6]
 }
--- cryptolog_test.go
+++ cryptolog_test.go
@@ -1,25 +1,48 @@
 package main
 
-import "testing"
+import (
+	"regexp"
+	"testing"
+)
 
 func TestHashIp(t *testing.T) {
-  salt = []byte("ueErQYkQp5A9LrNbRQ1+XQ==")
+	salt = []byte("ueErQYkQp5A9LrNbRQ1+XQ==")
 
-  ip := "172.17.0.1"
-  expected := "JKx+3b"
-  if got := hashIp(ip); got != expected {
-    t.Errorf("Expected %s, got %s", expected, got)
-  }
+	ip := "172.17.0.1"
+	want := "JKx+3b"
+	if got := hashIP(ip); got != want {
+		t.Errorf("want %s, got %s", want, got)
+	}
 }
 
 func TestProcessSingleLogEntry(t *testing.T) {
-  salt = []byte("ueErQYkQp5A9LrNbRQ1+XQ==")
-  r := compileRegexp(false)
+	salt = []byte("ueErQYkQp5A9LrNbRQ1+XQ==")
 
+	replaceAllRegexp := compileRegexp(true)
+	replaceOneRegexp := compileRegexp(false)
 
-  log_entry := `172.17.0.1 - - [29/Mar/2017:20:09:52 +0000] "GET / HTTP/1.1" 304 -`
-  expected := `JKx+3b - - [29/Mar/2017:20:09:52 +0000] "GET / HTTP/1.1" 304 -`
-  if got := processSingleLogEntry(log_entry, r); got != expected {
-    t.Errorf("Expected %s, got %s", expected, got)
-  }
+	var tests = []struct {
+		input string
+		want  string
+		r     *regexp.Regexp
+	}{
+		{`172.17.0.1 - - [29/Mar/2017:20:09:52 +0000] "GET / HTTP/1.1" 304 -`,
+			`JKx+3b - - [29/Mar/2017:20:09:52 +0000] "GET / HTTP/1.1" 304 -`,
+			replaceAllRegexp},
+		{`172.17.0.1 - - 172.17.0.1 - - [29/Mar/2017:20:09:52 +0000] "GET / HTTP/1.1" 304 -`,
+			`JKx+3b - - JKx+3b - - [29/Mar/2017:20:09:52 +0000] "GET / HTTP/1.1" 304 -`,
+			replaceAllRegexp},
+		{`172.17.0.1 - - [29/Mar/2017:20:09:52 +0000] "GET / HTTP/1.1" 304 -`,
+			`JKx+3b - - [29/Mar/2017:20:09:52 +0000] "GET / HTTP/1.1" 304 -`,
+			replaceOneRegexp},
+		{`172.17.0.1 - - 172.17.0.1 - - [29/Mar/2017:20:09:52 +0000] "GET / HTTP/1.1" 304 -`,
+			`JKx+3b - - 172.17.0.1 - - [29/Mar/2017:20:09:52 +0000] "GET / HTTP/1.1" 304 -`,
+			replaceOneRegexp},
+	}
+
+	for _, test := range tests {
+		if got := processSingleLogEntry(test.input, test.r); got != test.want {
+			t.Errorf("want %s, got %s", test.want, got)
+		}
+	}
 }
